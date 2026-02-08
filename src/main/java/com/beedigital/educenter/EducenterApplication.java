4c967ea9d81d00c8ed18c3f6239dd0918234ae1a
amdouni-bilel <amdounibilel1@gmail.com>
2026-02-08 21:16:23 +0100

    first commit

---
+++ src/main/java/com/beedigital/educenter/EducenterApplication.java
@@ -0,0 +1,13 @@
+package com.beedigital.educenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beedigital/educenter/EducenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beedigital.educenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
